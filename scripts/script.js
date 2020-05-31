cdfa08ebae25fe8fd04aace9aa4822dd8a7c62b3
Michael <michaelthreels@gmail.com>
2020-06-01 00:57:14 +0200

    added collapsable search section

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    var y = document.getElementById("topnav");
+    var z = document.getElementById("collaps");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      x.style.height = "65vh";
+      y.style.width = "100%";
+      z.style.transform = "rotate(180deg)"
+      z.style.padding = "0 0 12px 0";
+    } else {
+      x.style.display = "none";
+      x.style.height = "auto";
+      y.style.width = "45px";
+      z.style.transform = "none";
+      z.style.padding = "12px 0 0 0";
+    }
+  }
+
+// function scrollFunction() {
+//     var x = document.getElementById("myLinks");
+//         x.style.display = "none";
+// }
+
+document.getElementById("body").addEventListener("scroll", scrollFunction);
+
+function myFunction() {
+  document.getElementById("myLinks").style.display = "none";
+}
