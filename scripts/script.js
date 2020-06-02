59b029f625fd775b20d8d76c0dfc5628acb3b918
Michael <michaelthreels@gmail.com>
2020-06-02 22:54:38 +0200

    finished the search page including filter section

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,21 @@ function myFunction() {
     var x = document.getElementById("myLinks");
     var y = document.getElementById("topnav");
     var z = document.getElementById("collaps");
-    var a = document.getElementById("hot-ads")
+    var a = document.getElementById("hot-ads");
     if (x.style.display === "none") {
       x.style.display = "block";
       x.style.height = "65vh";
       y.style.width = "100%";
       z.style.transform = "rotate(180deg)"
-      z.style.padding = "0 0 12px 0";
-      a.style.margin = "66vh 0 0 0";
+      z.style.paddingBottom = "12px";
+      a.style.marginTop = "75vh";
     } else {
       x.style.display = "none";
       x.style.height = "auto";
       y.style.width = "45px";
       z.style.transform = "none";
-      z.style.padding = "12px 0 0 0";
-      a.style.margin = "15px 0 0 0";
+      z.style.paddingTop = "12px";
+      a.style.marginTop = "15px";
     }
   }
 
@@ -25,6 +25,7 @@ window.addEventListener("scroll", myScroll);
 function myScroll() {
     document.getElementById("myLinks").style.display = "none";
     document.getElementById("collaps").style.transform = "rotate(0deg)";
-    document.getElementById("collaps").style.padding = "12px 0 0 0";
-    document.getElementById("hot-ads").style.margin = "15px 0 0 0";
+    document.getElementById("collaps").style.paddingTop = "12px";
+    document.getElementById("hot-ads").style.marginTop = "15px";
+    document.getElementById("filter-section").style.marginTop = "15px";
 }
